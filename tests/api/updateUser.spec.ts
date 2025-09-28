b3b05fa3f1147dd2a930cfc25c16483e53c6e014
madans007 <madans007007@gmail.com>
2025-09-28 20:45:14 +0530

    file updates

--- tests/api/updateUser.spec.ts
+++ tests/api/updateUser.spec.ts
@@ -11,14 +11,25 @@ test.describe('JSON Placeholder API- Update User', () => {
 
         const updateUserPayload = getRandomUser();      // lets update payload for that user
         const updatedUserResponse: APIResponse = await request.put(`https://jsonplaceholder.typicode.com/users/${userId}`, { data: updateUserPayload });
-
         expect(updatedUserResponse.status()).toBe(200);
 
-        const updatedUserResponseBody:any = await updatedUserResponse.json();   // you'll get body here
-
+        const updatedUserResponseBody: any = await updatedUserResponse.json();   // body
         expect(updatedUserResponseBody).toHaveProperty('name', updateUserPayload.name);
         expect(updatedUserResponseBody).toHaveProperty('email', updateUserPayload.email);
-    
 
+    })
+
+
+    test('Partial update user with PATCH', async ({ request }) => {
+
+        const userId: number = 2;
+
+        const partilaUpdate: any = { email: 'patched@example.com' };
+        const patchResponse:APIResponse= await request.patch(`https://jsonplaceholder.typicode.com/users/${userId}`, { data: partilaUpdate })
+        expect(patchResponse.status()).toBe(200);
+
+        const patchResponseBody: any = await patchResponse.json();
+        console.log('Patched User:', patchResponseBody);
+        expect(patchResponseBody).toHaveProperty('email', "patched@example.com");
     })
 })
