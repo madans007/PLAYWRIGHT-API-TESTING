24e64beb3f7328b0bf11f78efd7ba9b3e6cb5906
madans007 <madans007007@gmail.com>
2025-10-02 20:23:07 +0530

    delete file update

--- tests/api/deleteUser.spec.ts
+++ tests/api/deleteUser.spec.ts
@@ -1,14 +1,12 @@
-import {test,expect, APIResponse} from '@playwright/test';
+import { test, expect, APIResponse } from '@playwright/test';
 
 
-test.describe('JSON Placeholder API - Delete User', async()=>{
+test.describe('JSON Placeholder API - Delete User', async () => {
 
-    test('Delete User by ID', async({request})=>{
+    test('Delete User by ID', async ({ request }) => {
 
-        const userId:number = 1;    // this is predefined user
-
-        const deleteResponse: APIResponse= await request.delete(`https://jsonplaceholder.typicode.com/users/${userId}`)
-
+        const userId: number = 1;    // this is predefined user
+        const deleteResponse: APIResponse = await request.delete(`https://jsonplaceholder.typicode.com/users/${userId}`)
         expect(deleteResponse.status()).toBe(200);
 
     })
