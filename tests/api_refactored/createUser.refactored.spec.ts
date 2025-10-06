5414cf3a9c10a9e23cf7122bde1407f4db779474
madans007 <madans007007@gmail.com>
2025-10-06 14:32:51 +0530

    createuser updates

---
+++ tests/api_refactored/createUser.refactored.spec.ts
@@ -0,0 +1,31 @@
+import { test } from '../../fixtures/baseTest';
+import { APIResponse, expect } from '@playwright/test';
+import { getRandomUser, getJsonUser } from '../../utils/dataProvider';
+
+
+
+test.describe('JSONPlaceholder API - Create User Refactored', () => {
+
+    test('Create User with Faker (dynamic) Refactored', async ({ userClient }) => {
+        const payload: any = getRandomUser();
+
+        const response: APIResponse = await userClient.createUser(payload);
+        expect(response.status()).toBe(201);             //accepted
+
+        const body = await response.json();             //body can be wriiten as responseBody as well
+        console.log('Created User (Faker):', body);
+        expect(body).toHaveProperty('id');
+    })
+
+    test('Create User with Static JSON Refactored', async ({ userClient }) => {
+        const payload: any = getJsonUser();
+
+        const response: APIResponse = await userClient.createUser(payload);
+        expect(response.status()).toBe(201);
+
+        const body = await response.json();
+        console.log('Created User (Static JSON):', body);
+        expect(body).toHaveProperty('id');
+    });
+
+})
