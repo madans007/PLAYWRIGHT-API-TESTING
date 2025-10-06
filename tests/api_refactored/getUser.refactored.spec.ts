fb3995610d7401ca7d691a1e2e7c901158e00e8f
madans007 <madans007007@gmail.com>
2025-10-06 16:48:21 +0530

    get  user updates

---
+++ tests/api_refactored/getUser.refactored.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '../../fixtures/baseTest';
+import { APIResponse, expect } from '@playwright/test';
+
+const predefinedUserId = 1; // JSONPlaceholder predefined user
+
+test.describe('JSONPlaceholder API - Get User Refactored', () => {
+
+    // Test for predefined user
+    test('Get User by ID Refactored', async ({ userClient }) => {
+        const response: APIResponse = await userClient.deleteUser(1);
+        const body: any = await response.json();
+        console.log('Fetched Predefined User:', body);
+        expect(body).toHaveProperty('id', predefinedUserId);
+    });
+});
