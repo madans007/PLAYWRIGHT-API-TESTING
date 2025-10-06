59f8ef35e98441b19e151151bff49e441b503a1a
madans007 <madans007007@gmail.com>
2025-10-06 16:44:28 +0530

    del user updates

---
+++ tests/api_refactored/deleteUser.refactored.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '../../fixtures/baseTest';
+import { APIResponse, expect } from '@playwright/test';
+
+
+
+test.describe('JSON Placeholder API - Delete User Refactored', async () => {
+
+    test('Delete User by ID Refactored', async ({ userClient }) => {
+
+        const userId: number = 1;    // this is predefined user
+        const deleteResponse: APIResponse = await userClient.deleteUser(1);
+        expect(deleteResponse.status()).toBe(200);
+
+    })
+
+})
