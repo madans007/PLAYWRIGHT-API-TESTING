edc209b9697a20faff11a4251e6625dd1d5323ea
madans007 <madans007007@gmail.com>
2025-10-14 00:04:55 +0530

    file updates

--- utils/env.ts
+++ utils/env.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'https://jsonplaceholder.typicode.com';
+import dotenv from 'dotenv';
+dotenv.config();                // it loads .env file
+
+export const BASE_URL = process.env.BASE_URL!; //reads the value from .env
